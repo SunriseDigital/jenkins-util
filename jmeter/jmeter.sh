101ab1b1bbe62c718d22809d3eb20a21340da274
Masamoto Miyata <miyata@sincere-co.com>
2013-12-18 14:23:25 +0900

    パイプでwhileを回した時、中身が別プロセスになって変数を参照できなかった問題を修正。 ファイルが0で書き出されることもあるようなので対応。 ログファイルの書き出し先を渡すオプションの名前をjutillogに変更。名前空間の問題。

--- jmeter/jmeter.sh
+++ jmeter/jmeter.sh
@@ -18,26 +18,38 @@ if [ $OPT_ERROR ]; then      # option error
 fi
 
 hasError=0
-find ${target_dir} -name "*.jmx" | while read jmx ; do
-  timekey=`date +%s%N`
-  hashkey=`head /dev/urandom -c128 | sha1sum | head -c12`
-  log_file=`printf "/tmp/ju-jmeter-%s-%s.log" "${timekey}" "${hashkey}"`
+#ここはパイプで{}ブロックに渡さないと中の処理が別プロセスで実行されるのでhasErrorの更新ができません。
+find ${target_dir} -name "*.jmx" | \
+{
+  while read -r jmx ; do
+    timekey=`date +%s%N`
+    hashkey=`head /dev/urandom -c128 | sha1sum | head -c12`
+    log_file=`printf "/tmp/ju-jmeter-%s-%s.log" "${timekey}" "${hashkey}"`
 
-  if [ -e ${log_file} ]; then
-    echo >&2 "Temp file ${log_file} is exists."
-    exit 1
-  fi
-
-  cmd="/bin/sh ${jmeter_home}/bin/jmeter -n -Jlogname=\"${log_file}\" ${jmeter_options}-t ${jmx}"
-  echo ${cmd}
-
-  eval ${cmd}\
-  && {
     if [ -e ${log_file} ]; then
-      hasError=1
-      cat ${log_file}
-      rm ${log_file}
+      echo >&2 "Temp file ${log_file} is exists."
       exit 1
     fi
-  }
-done
+
+    cmd="/bin/sh ${jmeter_home}/bin/jmeter -n -Jjutillog=\"${log_file}\" ${jmeter_options}-t ${jmx}"
+    echo ${cmd}
+
+    eval ${cmd}\
+    && {
+      # 容量が0じゃ無かったら
+      if [ -s ${log_file} ]; then
+        hasError=1
+        cat >&2 ${log_file}
+      fi
+      
+      # ファイルが存在したら
+      if [ -e ${log_file} ]; then
+        rm ${log_file}
+      fi
+    }
+  done
+
+  if [ $hasError -eq 1 ]; then
+    exit 1
+  fi
+}
